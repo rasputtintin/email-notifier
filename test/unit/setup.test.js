99754a3e3a77286cd9529f7e7299b8371d22a846
Valentin Genev <vgenev@gmail.com>
2019-01-25 09:57:07 +0200

    setup test added. not finished

--- test/unit/setup.test.js
+++ test/unit/setup.test.js
@@ -5,7 +5,9 @@
 const Sinon = require('sinon')
 const Test = require('tapes')(require('tape'))
 const Setup = require('../../src/setup').setup
-
+const Kafka = require('../../src/lib/kafka')
+const KafkaConsumer = require('@mojaloop/central-services-stream').Kafka.Consumer
+const Rx = require('rxjs')
 Test('Test the action observables Setup', setupTest => {
   /* let mockMessage = {}
 
@@ -17,5 +19,25 @@ Test('Test the action observables Setup', setupTest => {
   const source = m.hot('--^-a-b-|', inputs)
   const subs =           '^-----!'
   */
+  let sandbox
+  let ConsumerStub
+  let UtilityStub
+  let LoggerStub
+  let RxStub
+  let filterStub
+  let switchMapStub
+  let ObservablesStub
+  let healthcheckStub
 
+  setupTest.beforeEach(t => {
+    sandbox = Sinon.createSandbox()
+    sandbox.stub(KafkaConsumer.prototype, 'constructor').resolves()
+    sandbox.stub(KafkaConsumer.prototype, 'connect').resolves()
+    sandbox.stub(KafkaConsumer.prototype, 'consume').resolves()
+    sandbox.stub(KafkaConsumer.prototype, 'commitMessageSync').resolves()
+    sandbox.stub(Kafka.Consumer, 'getConsumer').returns({
+      commitMessageSync: async function () { return true }
+    })
+    sandbox.stub(Rx.prototype, 'Observable')
+  })
 })
